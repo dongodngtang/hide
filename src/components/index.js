af78e9da208809dabf4c63abdd458b0d4c52e62f
lorne <2270333671@qq.com>
2017-11-30 15:33:06 +0800

    清除不用包

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,6 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import {UltimateListView, UltimateRefreshView} from 'react-native-ultimate-listview';
 import ScrollTabView from 'react-native-scrollable-tab-view';
 import FastImage from 'react-native-fast-image';
-import Swiper from 'react-native-swiper';
 import NavTopBar from './NavTopBar';
 import WebViewPage from './WebViewPage';
 import HomeScrollTabBar from './HomeScrollTabBar';
@@ -17,7 +16,6 @@ export {
     UltimateListView,
     UltimateRefreshView,
     FastImage,
-    Swiper,
     NavTopBar,
     WebViewPage,
     HomeScrollTabBar,
